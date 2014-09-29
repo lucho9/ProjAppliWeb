d77fefdb6bcbf49dfa1765915ee7711d544dd09d
Sment <Sment@Zpeedy>
2014-09-29 02:05:46 +0200

    Customer

--- ProjAppliWeb/src/main/java/m2/project/controller/CustomerController.java
+++ ProjAppliWeb/src/main/java/m2/project/controller/CustomerController.java
@@ -1,57 +1,68 @@
 package m2.project.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import javax.validation.Valid;
 
 import m2.project.model.Customer;
 import m2.project.repository.CustomerRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Controller
-public class CustomerController {
+public class CustomerController extends WebMvcConfigurerAdapter {
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-	@RequestMapping(value = "/customer/create", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/customer", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+	public String customersList(Model model) {
+		model.addAttribute("customers", customerRepository.findAll());
+		return "/customer/list";
+	}
+
+	@RequestMapping(value = "/customer/create", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
 	public String createCustomerForm(Model model) {
 		model.addAttribute("customer", new Customer());
 		return "/customer/create";
 	}
 
 	@RequestMapping(value = "/customer/create", method = RequestMethod.POST)
-	public String submitCreateCustomerForm(@ModelAttribute Customer customer) {
+	public String submitCreateCustomerForm(@Valid @ModelAttribute Customer customer, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "redirect:/customer/create";
+		}
+
 		customerRepository.save(customer);
 		return "redirect:/customer";
 	}
 
-	@RequestMapping(value = "/customer", method = RequestMethod.GET)
-	public String customersList(Model model) {
-		model.addAttribute("customers", customerRepository.findAll());
-		return "/customer/list";
+	@RequestMapping(value = "/customer/edit", method = RequestMethod.GET)
+	public String editCustomerForm(@RequestParam("id") Long id, Model model) {
+
+		model.addAttribute("customer", customerRepository.findOne(id));
+		return "/customer/create";
+	}
+
+	@RequestMapping(value = "/customer/edit", method = RequestMethod.POST)
+	public String submitEditCustomerForm(@Valid @ModelAttribute Customer customer, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("customer", customer);
+			return "/customer/create";
+		}
+
+		customerRepository.save(customer);
+		return "redirect:/customer";
 	}
 
 	@RequestMapping(value = "/customer/delete", method = RequestMethod.GET)
 	public String deleteCustomer(@RequestParam("id") Long id) {
 		customerRepository.delete(id);
-		
-		return "redirect:/customer";
-	}
-
-	@RequestMapping(value = "/customer/edit", method = RequestMethod.GET)
-	public String editForm(@RequestParam("id") Long id, Model model) {
-
-		model.addAttribute("customer", customerRepository.findOne(id));
-		return "/customer/create";
-	}
-	
-	@RequestMapping(value = "/customer/edit", method = RequestMethod.POST)
-	public String submitEditForm(@ModelAttribute Customer customer) {
-
-		customerRepository.save(customer);
 		return "redirect:/customer";
 	}
 }
--- ProjAppliWeb/src/main/java/m2/project/model/Customer.java
+++ ProjAppliWeb/src/main/java/m2/project/model/Customer.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Customer {
@@ -12,7 +14,12 @@ public class Customer {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
+	@NotNull
+	@Size(min = 2, max = 30)
 	private String firstName;
+	
+	@NotNull
+	@Size(min = 2, max = 30)
 	private String lastName;
 
 	public long getId() {
