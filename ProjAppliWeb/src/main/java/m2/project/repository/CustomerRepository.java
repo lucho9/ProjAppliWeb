987c5ebf9e641dfa5e0826e9f33f71f063d8f174
lucien.banholzer <lucien.banholzer@wanadoo.fr>
2014-10-08 11:56:59 +0200

    Last Version of Product

--- ProjAppliWeb/src/main/java/m2/project/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package m2.project.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import m2.project.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
