bb3c584dc482f9ca53c219414290497f1dc35b0f
lucien.banholzer <lucien.banholzer@PCFSTB29.fst-miage.univ-mulhouse.fr>
2014-10-09 12:10:44 +0200

    test

--- ProjAppliWeb/src/main/java/m2/project/repository/CustomerGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package m2.project.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import m2.project.model.CustomerGroup;
-
-public interface CustomerGroupRepository extends CrudRepository<CustomerGroup, Long> {
-
-}
